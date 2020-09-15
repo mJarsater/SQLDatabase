7f941c5f1cecc50f4d3c6379a25037400aaf98c6
mjarsater <martin.jarsater@hotmail.com>
2020-09-15 15:43:08 +0200

    INSERT, ta hand om utskrift, toggle

--- src/SQLDatabase.java
+++ src/SQLDatabase.java
@@ -4,23 +4,25 @@ import com.mysql.jdbc.Connection;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-public class SQLDatabase extends JFrame{
+public class SQLDatabase extends JFrame {
+    private JFrame jFrame;
+    private SQLDatabase database;
     private SQLConnection connection;
     private JTextArea textArea;
-    private JLabel nameLabel, emailLabel , websiteLabel, commentLabel;
+    private JScrollPane scrollPane;
+    private JLabel nameLabel, emailLabel, websiteLabel, commentLabel;
     private JTextField nameField, emailField, websiteField, commentField;
-    private JButton addCommentBtn, refreshBtn;
+    private JButton addCommentBtn, refreshBtn, connectBtn;
     private String name, email, website, comment;
+    private String toggleConnect = "Connect";
 
-    public SQLDatabase(){
-        this.connection = new SQLConnection();
-        connection.start();
-
+    public SQLDatabase() {
+        this.database = this;
+        this.jFrame = this;
+        this.setTitle("No connection");
+        connectBtn = new JButton("Connect");
         addCommentBtn = new JButton("Add comment");
         refreshBtn = new JButton("Refresh");
         nameLabel = new JLabel("Name: ");
@@ -28,46 +30,52 @@ public class SQLDatabase extends JFrame{
         websiteLabel = new JLabel("Website: ");
         commentLabel = new JLabel("Comment: ");
         textArea = new JTextArea();
+        scrollPane = new JScrollPane(textArea);
+        textArea.setEditable(false);
         nameField = new JTextField();
         emailField = new JTextField();
         websiteField = new JTextField();
         commentField = new JTextField();
-        nameLabel.setBounds(10, 0,70,20 );
-        nameField.setBounds(80,0,100,20);
-        emailLabel.setBounds(10, 20,70,20 );
-        emailField.setBounds(80,20,100,20);
-        websiteLabel.setBounds(10, 40,70,20 );
-        websiteField.setBounds(80,40,100,20);
-        commentLabel.setBounds(10, 60,70,20 );
-        commentField.setBounds(80,60,100,20);
-        addCommentBtn.setBounds(200,40,150, 30);//x axis, y axis, width, height
-        refreshBtn.setBounds(200, 10,150,30);
-        textArea.setBounds(20,100, 340, 320);
+        nameLabel.setBounds(10, 0, 70, 20);
+        nameField.setBounds(80, 0, 100, 20);
+        emailLabel.setBounds(10, 20, 70, 20);
+        emailField.setBounds(80, 20, 100, 20);
+        websiteLabel.setBounds(10, 40, 70, 20);
+        websiteField.setBounds(80, 40, 100, 20);
+        commentLabel.setBounds(10, 60, 70, 20);
+        commentField.setBounds(80, 60, 100, 20);
+        refreshBtn.setBounds(200, 10, 100, 20);
+        refreshBtn.setEnabled(false);
+        addCommentBtn.setBounds(200, 35, 100, 20);
+        addCommentBtn.setEnabled(false);
+        connectBtn.setBounds(200, 60, 100, 20);
+        textArea.setBounds(20, 100, 340, 320);
+
+        String select = "SELECT * FROM Guestbook";
+        String entry = " INSERT INTO Guestbook (name, email, website, comment) " + "VALUES (?,?,?,?)";
 
         // -------------- BUTTONS ------------------------
+
+
         this.add(addCommentBtn);
-        addCommentBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textArea.setText("Clicked add comment");
-                getTextField();
-                String entry = " INSERT INTO Guestbook (name, email, website, comment) "+"VALUES (?,?,?,?)";
-               /* String entry = "CREATE TABLE Guestbook "+
-                        "(name VARCHAR(225), "+
-                        "email VARCHAR(225), "+
-                        "website VARCHAR(225), "+
-                        "comment VARCHAR(225))";*/
-                connection.writeToDb(entry, name, email, website, comment);
-            }
+        addCommentBtn.addActionListener(e -> {
+            textArea.setText("Clicked add comment");
+            getTextField();
+            connection.writeToDb(entry, name, email, website, comment);
         });
 
 
         this.add(refreshBtn);
-        refreshBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        refreshBtn.addActionListener(e -> connection.readFromDb(select));
 
-            }
+        this.add(connectBtn);
+        connectBtn.addActionListener(e -> {
+            connection = new SQLConnection(database);
+            connection.start();
+            jFrame.setTitle("Connected to server @" +connection.getUrl());
+            connectBtn.setText("Disconnect");
+            refreshBtn.setEnabled(true);
+            addCommentBtn.setEnabled(true);
         });
         // ------------ BUTTONS END -----------------------
 
@@ -82,22 +90,18 @@ public class SQLDatabase extends JFrame{
         this.add(commentField);
 
 
-
-
-        this.setSize(400,500);//400 width and 500 height
+        this.setSize(400, 500);//400 width and 500 height
         this.setLayout(null);//using no layout managers
         this.setVisible(true);//making the frame visible
 
     }
 
-    public void getTextField(){
+    public void getTextField() {
         name = nameField.getText();
         email = emailField.getText();
         website = websiteField.getText();
         comment = commentField.getText();
     }
-
-
 
     public static void main(String[] args) {
         SQLDatabase f = new SQLDatabase();
@@ -106,17 +110,33 @@ public class SQLDatabase extends JFrame{
     }
 
 
-
+    public void print(String name, String email, String website, String comment) {
+        textArea.setText(
+                "Name: "+name+ "\n"+ "Email: "+email+ "\n"+ "Website: "+website+ "\n"+ "Comment: "+comment
+        );
+    }
 }
 
 class SQLConnection extends Thread{
+    private SQLDatabase sqlDatabase;
     private boolean connected = false;
     private String url = "jdbc:mysql://atlas.dsv.su.se:3306/db_20947219";
     private String username = "usr_20947219";
     private String password = "947219";
-    private PreparedStatement statement;
+    private PreparedStatement prepStatement;
+    private Statement statement;
     private Connection dbConnection;
 
+
+    public SQLConnection(SQLDatabase sqlDatabase){
+        this.sqlDatabase = sqlDatabase;
+
+
+    }
+
+    public String getUrl(){
+        return url;
+    }
 
     public boolean isConnected(){
         return connected;
@@ -124,16 +144,43 @@ class SQLConnection extends Thread{
 
     public void writeToDb(String entry,String name,String email,String website,String comment) {
         try {
-            statement = dbConnection.prepareStatement(entry);
-            statement.setString(1,name);
-            statement.setString(2,email);
-            statement.setString(3,website);
-            statement.setString(4,comment);
-            statement.execute();
+            prepStatement = dbConnection.prepareStatement(entry);
+            prepStatement.setString(1,name);
+            prepStatement.setString(2,email);
+            prepStatement.setString(3,website);
+            prepStatement.setString(4,comment);
+            prepStatement.execute();
         } catch (SQLException sqlException){
             System.out.println(sqlException);
         }
     }
+
+    public synchronized void readFromDb(String select){
+        String name, email, website, comment;
+
+        try{
+        statement = dbConnection.createStatement();
+        ResultSet result = statement.executeQuery(select);
+
+        while(result.next()){
+            //Retrieve
+            name = result.getString("name");
+            email = result.getString("email");
+            website = result.getString("website");
+            comment = result.getString("comment");
+
+            sqlDatabase.print(name, email, website, comment);
+
+            System.out.println("Name: "+name);
+            System.out.println("Email:" +email);
+            System.out.println("Website: "+website);
+            System.out.println("Comment: "+comment);
+        }
+    } catch (SQLException sqle){
+            System.out.println(sqle);
+        }
+    }
+
 
 
     public void run(){
